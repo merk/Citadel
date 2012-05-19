d3910e128b2537123a45d7cdc16ca22c7f714e77
Tim Nagel <tim@nagel.com.au>
2012-05-19 20:04:26 +1000

    Capifony changes

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,21 +1,22 @@
-set :application,   "ozfortress"
-set :domain,        "ozfortress.com"
-set :deploy_to,     "/home/ozfortress/sites/#{domain}"
-set :app_path,      "app"
-set :user,          "ozfortress"
-set :use_sudo,      false
-set :keep_releases, 3
+set :application,         "ozfortress"
+set :domain,              "ozfortress.com"
+set :deploy_to,           "/home/ozfortress/sites/#{domain}"
+set :app_path,            "app"
+set :user,                "ozfortress"
+set :use_sudo,            false
+set :keep_releases,       3
 
-set :repository,  "git@github.com:merk/Citadel.git"
-set :scm,         :git
-set :deploy_via, :remote_cache
+set :repository,          "git@github.com:merk/Citadel.git"
+set :scm,                 :git
+set :deploy_via,          :remote_cache
 
-set :model_manager, "doctrine"
+set :model_manager,       "doctrine"
 
-role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain                         # This may be the same as your `Web` server
-role :db,         domain, :primary => true       # This is where Rails migrations will run
+role :web,                "demoman.ozfortress.com"
+role :app,                "demoman.ozfortress.com"
+role :db,                 "demoman.ozfortress.com", :primary => true
 
 set :dump_assetic_assets, true
-set :shared_files, ["app/config/parameters.ini"]
-# set :shared_children, []
+set :use_composer,        true
+set :shared_files,        ["app/config/parameters.ini"]
+# set :shared_children,   []
